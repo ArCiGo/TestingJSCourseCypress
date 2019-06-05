91064fb1521d1f9c08265ea3adf119dd1a39c4d1
ArCiGo <armandocifuentes_1@hotmail.com>
2019-06-05 16:49:32 -0500

    Add of 'authenticated calculator' test

--- cypress/e2e/calculator.js
+++ cypress/e2e/calculator.js
@@ -10,3 +10,21 @@ describe('anonymous calculator', () => {
     cy.getByTestId('total').should('have.text', '3')
   })
 })
+
+describe('authenticated calculator', () => {
+  it('displays the username', () => {
+    cy.createUser().then(user => {
+      cy.visit('/')
+
+      cy.getByText(/login/i).click()
+      cy.getByLabelText(/username/i).type(user.username)
+      cy.getByLabelText(/password/i).type(user.password)
+      cy.getByText(/submit/i).click()
+
+      cy.assertLoggedInAs(user)
+      cy.getByText(/logout/i).click()
+
+      cy.queryByTestId('username-display', {timeout: 300}).should('not.exist')
+    })
+  })
+})
